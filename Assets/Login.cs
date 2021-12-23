7e97caa267262a85d56c18306cbd0f295c393da6
JHernandez435 <69222698+JHernandez435@users.noreply.github.com>
2021-12-23 17:03:15 -0500

    Conexion_C#_php

---
+++ Assets/Login.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    [SerializeField] private GameObject m_registerUI = null;
+    [SerializeField] private GameObject m_loginUI = null;
+
+    public InputField UsernameInput;
+    public InputField PasswordInput;
+    public Button LoginButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoginButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(Main.Instance.Web.Login(UsernameInput.text, PasswordInput.text));
+        });
+    }
+
+    public void ShowLogin()
+	{
+		m_registerUI.SetActive(false);
+		m_loginUI.SetActive(true);
+
+	}
+	public void ShowRegister()
+	{
+		m_registerUI.SetActive(true);
+		m_loginUI.SetActive(false);
+
+	}
+
+}
---
+++ Assets/Web.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(Login("testuser" , "852"));
+    }
+
+    IEnumerator GetDate()
+    {
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("http://localhost/Game/GetDate.php"))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+
+                byte[] results = webRequest.downloadHandler.data;
+
+            }
+
+        }
+
+    }
+
+    /*IEnumerator GetUsers()
+    {
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("http://localhost/Game/GetUsers.php"))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+
+                byte[] results = webRequest.downloadHandler.data;
+
+            }
+
+        }
+
+    }*/
+
+    public IEnumerator Login(string username , string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post("http://localhost/Game/ajax/loginAjax.php", form))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+
+        }
+
+    }
+}
